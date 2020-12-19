1b22cced5782814f6b559131eaff83464471747c
Igor Silveira <igor.silveira@edirectinsure.com>
2020-12-19 09:40:31 +0000

    CSS file stylings

--- components/Card.js
+++ components/Card.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import styles from "./Card.module.css"
 
 export default function Card() {
     return (
-        <div className="">
-            <div className="">
-                <img src="avatar.png" className=""/>
+        <div className={`${styles.CardContainer} flex-row center`}>
+            <div className={styles.CardAvatarContainer}>
+                <img src="avatar.png" className={styles.CardAvatar}/>
             </div>
-            <div className="">
-                <p className="">Jhon Snow</p>
-                <span className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+            <div className={styles.CardTextContainer}>
+                <p className={styles.CardTextTitle}>Jhon Snow</p>
+                <span className={styles.CardTextBody}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
             </div>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,25 @@
 import Card from "../components/Card";
+import styles from "../styles/pages/index.module.css";
 
 function App() {
   return (
     <div className="">
-      <header className="">
-        <h1 className="">Welcome to <span className="">React Styling</span></h1>
-        <h2 className="">Lets learn the multiple ways that you can style your React components to create beautiful pages like this one.</h2>
-        <div className="">
-          <a className="" href="#cards">
+      <header className={`${styles.Header} full-height flex-col`}>
+        <h1 className="heading-1">Welcome to <span className="blue-text">React Styling</span></h1>
+        <h2 className={`${styles.MutedParagraph} heading-2 text-mute`}>Lets learn the multiple ways that you can style your React components to create beautiful pages like this one.</h2>
+        <div className="button-container">
+          <a className="button" href="#cards">
               Get Started
           </a>
         </div>
       </header>
-      <section id="cards" className="">
-        <h3 className="">Cards</h3>
-        <div className="">
+      <section id="cards" className={`${styles.CardSection} full-height flex-col center`}>
+        <h3 className="section-title heading-1">Cards</h3>
+        <div className="flex-row center">
           <Card />
           <Card />
         </div>
-        <div className="">
+        <div className="flex-row center">
           <Card />
           <Card />
         </div>
