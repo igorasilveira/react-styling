cb7be44a8e7bf2bdc957b6dfef0b23b10e731a5a
Igor Silveira <igor.silveira@edirectinsure.com>
2020-12-19 09:41:36 +0000

    centered avatar

--- components/Card.js
+++ components/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Card() {
     return (
-        <div className="bg-white rounded-md w-1/3 shadow-xl px-3 py-5 m-4 flex flex-row">
+        <div className="bg-white rounded-md w-1/3 shadow-xl px-3 py-5 m-4 flex flex-row items-center">
             <div className="flex-none">
                 <img src="avatar.png" className="h-28 w-28"/>
             </div>
